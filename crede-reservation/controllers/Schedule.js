d369e9231fc78445b61ad7e0ee7522c2ccec116f
ailtonbsj <ailton.ifce@gmail.com>
2018-07-08 00:40:00 -0300

    remove spaces

--- crede-reservation/controllers/Schedule.js
+++ crede-reservation/controllers/Schedule.js
@@ -8,14 +8,11 @@ $Schedule = Schedule.prototype;
 //properties
 $Schedule.primaryKeys = [];
 $Schedule.components = [];
-// $Schedule.modules = [];
-// $Schedule.views = [];
 $Schedule.moduleName = 'schedule';
 $Schedule.icon = 'fa-sliders';
 
 $Schedule.loadDataTable = function(){
 	var self = this;
-
 	self.listAll(function(res){
 		if(res){
 			var datasource = res.map(function(it){
@@ -29,11 +26,8 @@ $Schedule.loadDataTable = function(){
 			new Orkidea.Schedule('schedule', datasource,{});
 		}
 	});
-
 	$('#view-table-'+self.moduleName+' .overlay').addClass('hide');
 }
-
-// $Schedule.filteredBy = {};
 
 //autorun
 new Schedule();
