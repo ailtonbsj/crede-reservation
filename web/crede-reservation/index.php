a2c3d41216d8f7e996ac26c5e53ce44ded63584f
ailtonbsj <ailton.ifce@gmail.com>
2022-03-14 14:58:24 -0300

    change some meta tags

--- web/crede-reservation/index.php
+++ web/crede-reservation/index.php
@@ -1,7 +1,7 @@
 <?php
 
-if(!isset($_SESSION)) session_start();
-if(isset($_SESSION['user'])) {
+if (!isset($_SESSION)) session_start();
+if (isset($_SESSION['user'])) {
   header('Location: dash.php');
   exit();
 }
@@ -23,14 +23,39 @@ include 'strings.php';
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>
-    <?= $S['MainProjectName'] . ' ' . $S['ProjectName'] ?> | <?= $S['LogIn'] ?>
-  </title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <title><?= $S['MainProjectName'] . ' ' . $S['ProjectName'] ?> | <?= $S['LogIn'] ?></title>
+  <link rel="manifest" href="/manifest.json">
+
+  <meta name='mobile-web-app-capable' content='yes'>
+  <meta name='application-name' content='CREDE Reservas'>
+
+  <meta name='apple-mobile-web-app-capable' content='yes'>
+  <meta name='apple-mobile-web-app-status-bar-style' content='black'>
+  <meta name='apple-mobile-web-app-title' content='CREDE Reservas'>
+
+  <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
   <!-- Font Awesome -->
@@ -42,7 +67,7 @@ include 'strings.php';
   <!-- iCheck -->
   <link rel="stylesheet" href="./lib/icheck/skins/square/blue.css">
   <!-- Toastr -->
-  <link href="./lib/toastr/toastr.min.css" rel="stylesheet"/>
+  <link href="./lib/toastr/toastr.min.css" rel="stylesheet" />
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -54,119 +79,109 @@ include 'strings.php';
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
   <style type="text/css" media="screen">
-  	body {
-  		height: 80%;
-  	}
+    body {
+      height: 80%;
+    }
   </style>
-
-<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/manifest.json">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-<meta name="theme-color" content="#ffffff">
-
 </head>
+
 <body class="hold-transition login-page">
-<div class="login-box">
-  <div class="login-logo">
-    <a href="index.html"><b><?= $S['MainProjectName'] ?></b><?= $S['ProjectName'] ?></a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="login-box-body">
-    <p class="login-box-msg"><?= $S['MsgLogIn'] ?></p>
+  <div class="login-box">
+    <div class="login-logo">
+      <a href="index.html"><b><?= $S['MainProjectName'] ?></b><?= $S['ProjectName'] ?></a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+      <p class="login-box-msg"><?= $S['MsgLogIn'] ?></p>
 
-    <form>
-      <div class="form-group has-feedback">
-        <input id="user" type="text" class="form-control" placeholder="<?= $S['PlaceHolderLogin'] ?>">
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input id="pass" type="password" class="form-control" placeholder="<?= $S['PlaceHolderPass'] ?>">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> <?= $S['RememberMe'] ?>
-            </label>
+      <form>
+        <div class="form-group has-feedback">
+          <input id="user" type="text" class="form-control" placeholder="<?= $S['PlaceHolderLogin'] ?>">
+          <span class="glyphicon glyphicon-user form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+          <input id="pass" type="password" class="form-control" placeholder="<?= $S['PlaceHolderPass'] ?>">
+          <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
+        <div class="row">
+          <div class="col-xs-8">
+            <div class="checkbox icheck">
+              <label>
+                <input type="checkbox"> <?= $S['RememberMe'] ?>
+              </label>
+            </div>
           </div>
+          <!-- /.col -->
+          <div class="col-xs-4">
+            <button type="button" class="btn btn-primary btn-block btn-flat"><?= $S['SignIn'] ?></button>
+          </div>
+          <!-- /.col -->
         </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="button" class="btn btn-primary btn-block btn-flat"><?= $S['SignIn'] ?></button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
-    <!-- /.social-auth-links -->
+      </form>
+      <!-- /.social-auth-links -->
 
- <!--    <a href="#">I forgot my password</a><br>
+      <!--    <a href="#">I forgot my password</a><br>
     <a href="register.html" class="text-center">Register a new membership</a> -->
 
+    </div>
+    <!-- /.login-box-body -->
   </div>
-  <!-- /.login-box-body -->
-</div>
-<!-- /.login-box -->
+  <!-- /.login-box -->
 
-<!-- jQuery 3 -->
-<script src="./lib/jquery/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="./lib/bootstrap/js/bootstrap.min.js"></script>
-<!-- iCheck -->
-<script src="./lib/icheck/icheck.min.js"></script>
-<!-- Toastr -->
-<script src="./lib/toastr/toastr.min.js"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' /* optional */
-    });
+  <!-- jQuery 3 -->
+  <script src="./lib/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 3.3.7 -->
+  <script src="./lib/bootstrap/js/bootstrap.min.js"></script>
+  <!-- iCheck -->
+  <script src="./lib/icheck/icheck.min.js"></script>
+  <!-- Toastr -->
+  <script src="./lib/toastr/toastr.min.js"></script>
+  <script>
+    $(function() {
+      $('input').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' /* optional */
+      });
 
-	toastr.options = {
-	  "closeButton": true,
-	  "debug": false,
-	  "newestOnTop": false,
-	  "progressBar": true,
-	  "positionClass": "toast-top-right",
-	  "preventDuplicates": false,
-	  "onclick": null,
-	  "showDuration": "300",
-	  "hideDuration": "1000",
-	  "timeOut": "3000",
-	  "extendedTimeOut": "1000",
-	  "showEasing": "swing",
-	  "hideEasing": "linear",
-	  "showMethod": "fadeIn",
-	  "hideMethod": "fadeOut"
-	}
+      toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "3000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+      }
 
-	$('.btn-primary').click(function(){
-        $.post('api/login',{
-            'name':$('#user').val(),
-            'pass':$('#pass').val()
-        }, function(res){
-        	console.log(res);
-        	if(JSON.parse(res)['status'] == 'success') window.location = 'dash.php';
-        	else toastr["error"]("<?= $S['InvalidLogIn'] ?>", "<?= $S['Error'] ?>");
+      $('.btn-primary').click(function() {
+        $.post('api/login', {
+          'name': $('#user').val(),
+          'pass': $('#pass').val()
+        }, function(res) {
+          console.log(res);
+          if (JSON.parse(res)['status'] == 'success') window.location = 'dash.php';
+          else toastr["error"]("<?= $S['InvalidLogIn'] ?>", "<?= $S['Error'] ?>");
         });
+      });
+
     });
 
-  });
-</script>
+    if (navigator.serviceWorker != null) {
+      navigator.serviceWorker.register('/sw.js')
+        .then(function(registration) {
+          console.log('Registered events at scope: ', registration.scope);
+        });
+    }
+  </script>
 </body>
+
 </html>
